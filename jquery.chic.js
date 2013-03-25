7588bc0dedb0e4c1333cce21f96daba428a70a3e
Anthony Ettinger <anthony@chovy.com>
2013-03-24 18:53:50 -0700

    few modifications as suggested by Brito

--- jquery.chic.js
+++ jquery.chic.js
@@ -3,37 +3,37 @@
    JQuery plugin for Styled Selects & Checkboxes
    
    Jonathan Dallas
+   Anthony Ettinger (a few modifications)
 */
-
 
 ;(function($) {
    $.fn.chic = function() {
     return this.each(function() {
       var $el = $(this);
-      if (this.tagName == 'SELECT' && $el.parents('.chic-selector').length == 0 ) {
+
+      if ( this.tagName == 'SELECT' && !$el.parents('.chic-selector').length ) {
         $el
          .css({ 'z-index': 10, opacity: 0, '-khtml-appearance': 'none' })
          .wrap('<div class=chic-selector>')
          .before($('<span>').text($el.find(':selected').text()))
          .change(function() {
-           val = $('option:selected', this).text();
+           var val = $('option:selected', this).text();
            $el.prev().text(val);
          });
-      };
-      if ($el.is('input[type=checkbox]') && $el.parents('.chic-checkbox').length == 0 ) {
+      }
+
+      if ( $el.is('input[type=checkbox]') && !$el.parents('.chic-checkbox').length ) {
         $el
          .css({ 'z-index': 10, opacity: 0, '-khtml-appearance': 'none' })
          .wrap('<div class=chic-checkbox>')
          .removeClass('chic')
          .change(function() {
-           if ($el.is(':checked')) { $el.parent().addClass('checked');    
-              $el.parents('label').addClass('checked') }
-           else { $el.parent().removeClass('checked');
-              $el.parents('label').removeClass('checked') }
+           var checked = $el.is(':checked');
+
+           $el.parent()[ checked ? 'addClass' : 'removeClass' ]('checked');
+           $el.parents('label')[ checked ? 'addClass' : 'removeClass' ]('checked');
          });
-        if ($el.is(':checked')) { $el.parent().addClass('checked');
-           $el.parents('label').addClass('checked') }
-      };
+      }
     });
   };
 })(jQuery);
